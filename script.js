ac6b1751ee345027d246b409f7b8195882abdf41
Ayubxon2009 <abduganieva200980@gmail.com>
2025-02-07 15:23:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+    let name = prompt( `Ismingizni kiriting`)  
+
+    let firstname = prompt( `Familiyangizni kiriting`)
+
+    let school = prompt( `Nechinchi maktabda o'qiysiz`)
+
+    let mode = prompt( `mode_me_id`)
+     
+    let yosh = prompt( `Yoshingizni kiriting`)
+
+    let code = prompt(`code ni kiriting `)
+
+
+    console.log(name);
+    console.log(firstname);
+    console.log(school);
+    console.log(mode);
+    console.log(yosh);
+    console.log(code);
+    
